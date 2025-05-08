8366347b78a6095670121f837383e7235a45fde9
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-05-08 18:48:44 +0800

    refactor: enhance error handler to use logger for error logging

--- src/infrastructure/middleware/error-handler.ts
+++ src/infrastructure/middleware/error-handler.ts
@@ -1,25 +1,28 @@
 import { Response, Request, NextFunction } from 'express'
 import { AppError, InternalServerError } from '../../interfaces/http/errors'
+import { Logger } from '../../application/shared'
 
-export const errorHandler = (
-    error: unknown,
-    request: Request,
-    response: Response,
-    next: NextFunction
-) => {
-    if (error instanceof AppError) {
-        console.log(error)
-        response.status(error.statusCode).json({
-            name: error.name,
-            message: error.message,
-        })
-        return
-    } else {
-        console.error('Unexpected error:', error)
-        const internalError = new InternalServerError()
-        response.status(internalError.statusCode).json({
-            name: internalError.name,
-            message: internalError.message,
-        })
+export function errorHandler(logger: Logger) {
+    return (
+        error: unknown,
+        _request: Request,
+        response: Response,
+        _next: NextFunction
+    ) => {
+        if (error instanceof AppError) {
+            logger.debug(error.message, { error })
+            response.status(error.statusCode).json({
+                name: error.name,
+                message: error.message,
+            })
+            return
+        } else {
+            logger.error('Unexpected error', { error })
+            const internalError = new InternalServerError()
+            response.status(internalError.statusCode).json({
+                name: internalError.name,
+                message: internalError.message,
+            })
+        }
     }
 }
